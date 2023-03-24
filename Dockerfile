6bed5382b79ee64179c78f568fcfb4458c366ef6
Marcos Della <mdella@gmail.com>
2023-03-24 06:49:56 +0000

    Updated for environment variables

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,8 @@ ARG PYTHON3_VERSION="3.10"
 FROM docker.io/python:${PYTHON3_VERSION}
 
 RUN pip install --upgrade pip \
-  && pip install django 
-  #&& pip install psycopg2-binary
+  && pip install django \
+  && pip install psycopg2-binary
 
 COPY mysite /mysite
 WORKDIR /mysite
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,21 +75,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'mysite',
-#        'USER': 'postgres',
-#        'PASSWORD': 'django',
-#        'HOST': 'postgres',
-#    },
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 #}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
